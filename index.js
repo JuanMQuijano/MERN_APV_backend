5fc53c5441688d9e2f2efbac0c9dd8719fa4a324
Juan Q <jmquijano@unimayor.edu.co>
2023-06-16 21:40:32 -0500

    Cambio en Cors

--- index.js
+++ index.js
@@ -33,6 +33,19 @@ const corsOptions = {
   },
 };
 
+/** CORS setting with OPTIONS pre-flight handling */
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, accept, access-control-allow-origin"
+  );
+
+  if ("OPTIONS" == req.method) res.sendStatus(200);
+  else next();
+});
+
 app.use(cors({ origin: "https://mern-apv-frontend-sand.vercel.app" }));
 
 //Permitir el envío de datos
