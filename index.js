cc6341cd8fa40c346f2b210728912482b49b6e78
Juan Q <jmquijano@unimayor.edu.co>
2023-06-16 21:49:22 -0500

    Cambio en Cors

--- index.js
+++ index.js
@@ -20,20 +20,20 @@ const app = new express();
 //Conectamos la BD
 conectarDB();
 
-/** CORS setting with OPTIONS pre-flight handling */
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, accept, access-control-allow-origin"
-  );
+const dominiosPermitidos = ["https://mern-apv-frontend-sand.vercel.app"];
 
-  if ("OPTIONS" == req.method) res.sendStatus(200);
-  else next();
-});
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      //El origen del request está permitido
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: corsOptions }));
 
 //Permitir el envío de datos
 app.use(express.json());
